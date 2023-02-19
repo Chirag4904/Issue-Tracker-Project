0cb97675df776053d6bd74278c0c11db91a2beb8
chiggy69 <chiragaggarwal69@gmail.com>
2023-02-19 14:46:53 +0530

    Add issue button

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -2,6 +2,7 @@ import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { useState } from "react";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 export default function Issues() {
 	const [labels, setLabels] = useState([]); //shared state in issueslist and labelslist to know selected label
 	const [status, setStatus] = useState(""); //shared state in issueslist and statusselect to know selected status
@@ -30,6 +31,10 @@ export default function Issues() {
 						value={status}
 						onChange={(event) => setStatus(event.target.value)}
 					/>
+					<hr />
+					<Link className="button" to="/add">
+						Add Issue
+					</Link>
 				</aside>
 			</main>
 		</div>
